86e39d91c5dbeb427838191f290386a12b581dc4
K.Domke <kenneth.domke@jaemacom.de>
2025-08-05 16:46:15 +0200

    some styling change

--- public/js/filterCards.js
+++ public/js/filterCards.js
@@ -96,10 +96,15 @@ function updateSort() {
 
 function toggleDirection() {
     const directionBtn = document.getElementById("sort-direction");
+    const icon = document.getElementById("sort-icon");
+
     const current = directionBtn.dataset.direction;
     const next = current === "asc" ? "desc" : "asc";
     directionBtn.dataset.direction = next;
-    directionBtn.textContent = next === "asc" ? "↑" : "↓";
+
+    if (icon) {
+        icon.setAttribute("name", next === "asc" ? "ci:sort-ascending" : "ci:sort-descending");
+    }
 
     updateSort();
 }
